2b42deaf7cc0dd1bdf960e3992e88a8f4c7bd62b
sdmilineam <sdmilineam@gmail.com>
2021-07-13 14:15:59 +0200

    finis uploader

--- src/Entity/Profil.php
+++ src/Entity/Profil.php
@@ -6,10 +6,13 @@ use App\Repository\ProfilRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=ProfilRepository::class)
  * @ORM\HasLifecycleCallbacks()
+ * @Vich\Uploadable
  */
 class Profil
 {
@@ -64,6 +67,19 @@ class Profil
      * @ORM\Column(type="string", length=255)
      */
     private $link;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Image;
+
+    /**
+     *  @Vich\UploadableField(mapping="image", fileNameProperty="image")
+     * @ORM\Column(type="string", length=255)
+     *
+     * @var null|File
+     */
+    private $imageFile;
 
     public function __construct()
     {
@@ -199,5 +215,41 @@ class Profil
         $this->link = $link;
 
         return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(?string $Image): self
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of mediaFile.
+     *
+     * @return null|File
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param null|File|\Symfony\Component\HttpFoundation\File\UploadedFile $imageFile
+     */
+    public function setImageFile(?File $image = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updated_at = new \DateTimeImmutable();
+        }
     }
 }
--- src/Form/ProfilType.php
+++ src/Form/ProfilType.php
@@ -6,6 +6,7 @@ use App\Entity\Profil;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class ProfilType extends AbstractType
 {
@@ -18,6 +19,13 @@ class ProfilType extends AbstractType
             ->add('updateAt')
             ->add('commentaire')
             ->add('link')
+            ->add('imageFile', VichFileType::class, [
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => 'remove Photo',
+                'download_uri' => true,
+                'download_label' => 'Download Photo',
+            ])
         ;
     }
 
