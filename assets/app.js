74810300511cf835988b6f2fab355b8cd58b6776
sdmilineam <sdmilineam@gmail.com>
2021-07-22 14:40:00 +0200

    14:39 22/07

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,9 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+// require jQuery normally
+const $ = require('./jquery.js');
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,5 @@
 (function($){
- 
-    $(".icon").click(function(){
-      $("ul").slideToggle("slow");
-    });
-    
-    
-    $(".prev").click(function(){
-      $('.slidershow').animate({left: '+=100vw' },"slow");
-    });
-    
-    $(".next").click(function(){
-      $('.slidershow').animate({left: '-=100vw' },"slow");
-    });  
-  
-  
-  //   $('input').click(function() {
-  //     $('#destination').val($(this).html());
-  //  });
-  
-  
-  
-  
-  
-  
-  
-  })(jQuery);
+
+    console.log('hello');
+
+})(jQuery);
