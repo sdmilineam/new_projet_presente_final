8b979046c50f14fd268919541b72b0c69ad6e029
sdmilineam <sdmilineam@gmail.com>
2021-07-28 22:33:03 +0200

    final

--- assets/responsive.js
+++ assets/responsive.js
@@ -1,17 +1,12 @@
 (function($){
    
-    $(".menu").slideToggle("slow", function(){
-        $("i").toggleClass("fa-bars");
-         $("i").toggleClass("fa-times");
-    });
     $('#burger').on('click', function() {
         $(".menu").slideToggle("slow", function() {
             $("i").toggleClass("fa-bars");
-            $("i").toggleClass("fa-times");
-           
+             $("i").toggleClass("fa-times");
         });
-        
     });
+    
     
 
 
